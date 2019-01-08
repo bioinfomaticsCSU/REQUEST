865b2fe900d493c73d3a9e51a9e6c15219134b8d
bioinfomaticsCSU <luojunwei@csu.edu.cn>
2019-01-08 09:27:19 +0800

    Add files via upload

---
+++ fa2fa.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from sys import argv
+
+def fa2fa(org,out):
+    fin=open(org,"r")
+    fout=open(out,"w")
+    seq=""
+    while 1:
+        line=fin.readline()
+        if not line:
+            break
+        if line.startswith(">"):
+            if seq!="":
+                fout.write(seq+"\n")
+            seq=""
+            fout.write(line)
+        else:
+            line=line.strip("\n")
+            seq+=line
+    fout.write(seq+"\n")
+    fin.close()
+    fout.close()
+
+if __name__=="__main__":
+    org=argv[1]
+    out=argv[2]
+    fa2fa(org,out)
+
+
+
